a1fe4c00a337c577f9ae3b5a7262a27fb3012949
mahmadifarpooyagroup-png <m.ahmadifar.pooyagroup@gmail.com>
2026-08-10 16:02:48 +0330

    feat: add root Frappe app hooks

---
+++ atrin/hooks.py
@@ -0,0 +1,8 @@
+app_name = "atrin"
+app_title = "Atrin Smart Service Platform"
+app_publisher = "Atrin"
+app_description = "Smart service platform built on Frappe Framework"
+app_email = ""
+app_license = "License to be determined"
+
+# Keep Atrin domains modular while sharing the Frappe runtime.
